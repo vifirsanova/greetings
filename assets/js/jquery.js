e1c542271fc06bfd4fff2f44264db0de3915b54a
Виктория Фирсанова <vifirsanova@gmail.com>
2023-03-11 12:07:04 +0300

    add color switch toggle

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -14,3 +14,9 @@ function Scrollback() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('light');
+})
